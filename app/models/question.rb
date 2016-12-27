2b1712226ef1115698c588993c9c99c4ace637d0
vileven <vileven11@icloud.com>
2016-12-27 09:06:23 +0300

    Fix postgres bag

--- app/models/question.rb
+++ app/models/question.rb
@@ -17,7 +17,7 @@ class Question < ApplicationRecord
   def self.search(search)
     if search
       # Item.where('game_name LIKE :search OR genre LIKE :search OR console LIKE :search', search: "%#{search}%")
-      Question.joins(:tags).where('title LIKE ? OR tags.name LIKE ?', "%#{search}%", "#{search}")
+      Question.joins(:tags).where('title LIKE ? OR tags.name LIKE ?', "%#{search}%", "#{search}").uniq
     else
       Question.all
     end
